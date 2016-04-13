46d9394f8b6b4f6832882fcdf95afe0dee8f5567
Jesus Velasco <jesus.velasco@babel.es>
2016-04-13 13:05:25 +0200

    Reutilizaicion con componentes web

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var cont = 0;
-root.querySelector('#btnUp').addEventListener("click", function(){
-    cont = cont + 1;
-    root.querySelector('#spam').textContent = cont;
-    console.log("clickea 1. Valor:", cont);
-});
-root.querySelector('#btnDw').addEventListener("click", function(){
-    cont = cont - 1;
-    root.querySelector('#spam').textContent = cont;
-    console.log("clickea 2. Valor:", cont);
-});
+function createNew(container, image, body) {
+    var root = document.querySelector(container).createShadowRoot();
+    var template = document.querySelector("#news").content;
+    template.querySelector.(".body").textContent = body;
+    template.querySelector.(".image").textContent = image;
+    root.append();
+}
+createNew();
+createNew();
